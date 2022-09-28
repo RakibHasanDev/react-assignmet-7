577c69d9cef4e03c0f465a0957c7b9e94c284e0e
MrMernDeveloper <rakibkst16@gmail.com>
2022-09-29 02:33:41 +0600

    complete ui

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -7,7 +7,26 @@ const Person = () => {
     return (
         <div className='person-section'>
             <PersonInfo></PersonInfo>
-       </div> 
+            <h4>Add A Break</h4>
+            <div className='person-break'>
+                <div>
+                    <button>10s</button>
+                    <button>20s</button>
+                    <button>30s</button>
+                    <button>40s</button>
+                </div>
+            </div>
+            <div><h3>Exercise Details</h3></div>
+            <div className='exercise-time'>
+                <h4>Exercise time</h4>
+                <p>seconds</p>
+            </div>
+            <div className='break-time'>
+                <h4>Break time</h4>
+                <p>seconds</p>
+
+            </div>
+        </div>
     );
 };
 
