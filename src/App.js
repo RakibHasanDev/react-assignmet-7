1ca623136e584a73829d757946691dfd97a8429e
MrMernDeveloper <rakibkst16@gmail.com>
2022-09-28 23:37:20 +0600

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-import Nav from './components/Nav/Nav';
+import Home from './components/Home/Home';
+
 
 function App() {
   return (
     <div>
-      <Nav></Nav>
+     <Home></Home>
 
      
     </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,33 @@
 import React from 'react';
 
-
+import logo from '../../Images/1199-gym-01.png'
+import "./Home.css"
 
 const Home = () => {
+
+    
     return (
         <div className='home'>
 
-            <div className='Exercises'>
+            <div className='left'>
+                <div className='nav-title'>
+                    <img src={logo} alt="" />
+                    <div>
+                        <h1>GYM-ZONE-BD</h1>
+                    </div>
+                </div>
+                <h1>Select today’s exercise</h1>
 
+                <div exercise-container>
+
+                </div>
+
+               
 
             </div>
 
-            <div className='personal-info'>
+            <div className='right'>
+                <h1>this is right</h1>
 
             </div>
         </div>
